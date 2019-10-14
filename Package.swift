5970ac5fa056e9a1f6c920eece97eb908006a7bc
Yannick Weiss <contact@yannickweiss.com>
2019-10-14 17:24:13 +0200

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(name: "UICircularProgressRing",
+                      platforms: [.iOS(.v8)],
+                      products: [.library(name: "UICircularProgressRing",
+                                          targets: ["UICircularProgressRing"])],
+                      targets: [.target(name: "UICircularProgressRing",
+                                        path: "src")],
+                      swiftLanguageVersions: [.v5])
