506b04411f9adfd0d45de4312b3afdef8eae0a61
Yannick Weiss <contact@yannickweiss.com>
2019-10-14 17:47:59 +0200

    imported uikit

--- Package.swift
+++ Package.swift
@@ -1,9 +1,21 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
-let package = Package(name: "UICircularProgressRing",
-platforms: [.iOS(.v8)],
-products: [.library(name: "UICircularProgressRing", targets: ["UICircularProgressRing"])],
-targets: [.target(name: "UICircularProgressRing", path: "src/UICircularProgressRing")],
-swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "UICircularProgressRing",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "UICircularProgressRing",
+            targets: ["UICircularProgressRing"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "UICircularProgressRing",
+            path: "src/UICircularProgressRing")
+            dependencies: []),
+    ]
+)
